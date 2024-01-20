f257b88b25cb7a5251bad7c5fd8b6d473f9c4732
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-01-19 17:51:11 -0700

    name change

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 // static files access
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 const PORT = process.env.PORT || 8000;
 
