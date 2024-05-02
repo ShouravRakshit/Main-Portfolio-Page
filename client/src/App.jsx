ee945db948530aa469a5c0dd45154407d9b0bd70
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-05-01 22:31:58 -0600

    hopefully fixed the issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,15 @@ import Footer from "./Pages/Footer/Footer";
 import TechStack from "./Pages/TechStack/TechStack";
 import ScrollToTop from "react-scroll-to-top";
 import { useTheme } from "./Context/ThemeContext";
+// import "./Pages/Projects/projects.css";
+
 import "./app.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Summarify from "../src/assets/images/Summarify.png";
+import Tokyo from "../src/assets/images/Tokyo Olympic 2021 Analysis.png";
+import Atliq from "../src/assets/images/Atliq Sales Analysis.png";
+import Jump from 'react-reveal/Jump';
 
 function App() {
   const [theme] = useTheme();
@@ -35,13 +41,115 @@ function App() {
         <About color={theme} />
         <Education color={theme} />
         <TechStack color={theme} />
-        <div style={{ backgroundColor: "white", textAlign: "center" }}>
-          <h1>Projects</h1>
-
-          <ol>
-            <li>Food</li>
-            <li>Racing Car</li>
-          </ol>
+        <div style={{  textAlign: "center" }}>
+        <Jump > 
+      <div style={{ width: "75%" }} className="container" id="projects">
+        <div className="container text-center mt-3 project">
+          <h1 style={{ textTransform: "uppercase",  }}>My Projects</h1>
+        </div>
+        {/* card design */}
+        <div className="row" id="ads">
+          <div className="col-md-6">
+            <div className="card rounded">
+              <div className="card-image">
+                <a
+                  href="https://github.com/the-sin14/seng-513-project"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={Summarify} alt="Summarify" />
+                </a>
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">Typescript</span>
+                <span className="card-detail-badge">Express</span>
+                <span className="card-detail-badge">React</span>
+                <span className="card-detail-badge mb-2">MongoDB</span>
+                <span className="card-detail-badge">Docker</span>
+                <span className="card-detail-badge">Material-UI</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">Summarify</h5>
+                </div>
+                <a
+                  className="ad-btn"
+                  target="_black"
+                  href="https://github.com/the-sin14/seng-513-project?tab=readme-ov-file"
+                >
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card rounded">
+              <div className="card-image">
+                <a
+                  href="https://github.com/ShouravRakshit/Tokyo-Olympic-2021"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={Tokyo} alt="Tokyo Olympic" />
+                </a>
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">Microsoft Azure</span>
+                <span className="card-detail-badge">SQL</span>
+                <span className="card-detail-badge mb-2">Apache Spark</span>
+                <span className="card-detail-badge">Power BI</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">
+                    Tokyo Olympic 2021 Data Analysis
+                  </h5>
+                </div>
+                <a
+                  className="ad-btn"
+                  target="_black"
+                  href="https://github.com/ShouravRakshit/Tokyo-Olympic-2021"
+                >
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card rounded">
+              <div className="card-image">
+                <a
+                  href="https://github.com/ShouravRakshit/Atliq-Company-Sales-Analysis"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={Atliq} alt="Atliq Sales" />
+                </a>
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">MySQL</span>
+                <span className="card-detail-badge">Kaggle</span>
+                <span className="card-detail-badge mb-5">Power BI</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">
+                    Atliq Company Sales Analysis
+                  </h5>
+                </div>
+                <a
+                  className="ad-btn"
+                  target="_black"
+                  href="https://github.com/ShouravRakshit/Atliq-Company-Sales-Analysis"
+                >
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Jump>
         </div>
 
         {/* <Projects color={theme} /> */}
